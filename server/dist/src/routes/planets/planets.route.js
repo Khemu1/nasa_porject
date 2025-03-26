be8eddce03bc366fdbd5d85a2e643f85b6dc0032
khemu <alghost2004@gmail.com>
2025-03-26 16:35:58 +0200

    .

--- server/dist/src/routes/planets/planets.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.planetRouter = void 0;
-const express_1 = require("express");
-const dpi_1 = require("../../DPI/dpi");
-exports.planetRouter = (0, express_1.Router)();
-exports.planetRouter.get("/", dpi_1.planetController.getAllPlanets);
